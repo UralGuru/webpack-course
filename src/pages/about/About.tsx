5a16dc6a989bbad1f01b9d41684db6738fa71b05
Ural Guru <gizzatullinu@gmail.com>
2023-12-11 12:58:58 +0500

    Assets | PNG | JPG | SVG(svgr)

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,7 +1,18 @@
+import treePng from '@/assets/tree.png';
+import christmasJpg from '@/assets/christmas.jpg';
+
 const About = () => {
   return (
     <>
         About
+        <div>
+          <img src={treePng} alt="Tree" />
+        </div>
+        {treePng}
+        <div>
+          <img src={christmasJpg} alt="christmasJpg" />
+        </div>
+        {christmasJpg}
     </>
   );
 };
--- src/pages/shop/Shop.tsx
+++ src/pages/shop/Shop.tsx
@@ -1,7 +1,12 @@
+import Present from '@/assets/present.svg';
+
 const Shop = () => {
   return (
     <>
         Shop
+        <div>
+          <Present style={{color: 'red'}} width={150} height={150}/>
+        </div>
     </>
   );
 };
